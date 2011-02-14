365ef12adc2c4f717643b9228364c590c2c1648a
James D. Nurmi <james@abneptis.com>
2011-02-14 13:57:55 -0800

    - Make public a cachable value (so as to allow faster validation) - Clarify that usage is in bytes - re-privatize secret (was only public for debugging. TODO: Copy bigs rather than returning internal pointers?

--- src/pkg/diffie.go
+++ src/pkg/diffie.go
@@ -7,27 +7,33 @@ import "io"
 type DHData struct {
   P *big.Int
   G *big.Int // generally, actually a very small number.
-  S *big.Int // "Secret" integer
+  s *big.Int // "Secret" integer
+  public *big.Int
 }
 
+/* NB: L is in BYTES not BITS.  You really don't want to wait for a
+2048 BYTE DH keyex.  */
 func NewDH(r io.Reader, l int, g *big.Int, p *big.Int)(dhd *DHData, err os.Error){
   buff := make([]byte, l)
   n, err := r.Read(buff)
   if n < l && err == nil { err = os.NewError("Random source provided insufficent data") }
   if err != nil { return }
-  dhd = &DHData {P: p, G: g, S: big.NewInt(0) }
+  dhd = &DHData {P: p, G: g, s: big.NewInt(0) }
   // Use random data for our secret
-  dhd.S.SetBytes(buff)
+  dhd.s.SetBytes(buff)
   // But it might be negative, so we Abs it.
-  dhd.S.Abs(dhd.S)
+  dhd.s.Abs(dhd.s)
   return
 }
 
 // Based on the secret generated at creation (as well as
 // G&P)
 func (self *DHData)ComputePublic()(q *big.Int){
-  q = big.NewInt(0)
-  q.Exp(self.G, self.S, self.P) 
+  if self.public == nil {
+    self.public = big.NewInt(0)
+    self.public.Exp(self.G, self.s, self.P) 
+  }
+  q = self.public
   return
 }
 
@@ -50,6 +56,6 @@ func (self DHData)ComputeShared(in *big.Int)(out *big.Int, err os.Error){
   if chk.Cmp(big.NewInt(1)) != 0 {
     err = os.NewError("Invalid DH Key (shape)")
   }*/
-  out.Exp(in, self.S, self.P)
+  out.Exp(in, self.s, self.P)
   return
 }
